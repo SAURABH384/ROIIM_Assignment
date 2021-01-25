ad53534f72519e3433790d148148a4359f296d91
SAURABH <saurabhtiwari384@gmail.com>
2021-01-25 22:21:43 +0530

    Update CheckoutController.java

--- src/main/java/com/ROIIM/Assignment/controller/CheckoutController.java
+++ src/main/java/com/ROIIM/Assignment/controller/CheckoutController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(maxAge = 3600)
 public class CheckoutController {
 
     @Autowired
